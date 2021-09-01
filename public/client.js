43feab889ce52843311cc2ea912a3ae6e5637e60
Evgeniy <ekukonen@gmail.com>
2021-09-01 19:43:11 +0300

    add share messages

--- public/client.js
+++ public/client.js
@@ -1,13 +1,24 @@
+const messagesElement = document.getElementById("Messages")
 const textElement = document.getElementById("Input-text")
 const buttonElemet = document.getElementById("Input-button")
 let text = ""
 
 const ws = new WebSocket('ws://localhost:8080')
 
-buttonElemet.addEventListener('click', () => {
+buttonElemet.addEventListener('click', event => {
+    event.preventDefault();
     text = textElement.value
+    ws.send(text)
+    textElement.value = ""
 })
 
 ws.onopen = () => setStatus('ONLINE')
 ws.onclose = () => setStatus('DISCONECTED')
-ws.onmessage = response => console.log(response.data)
+ws.onmessage = async (response) => {
+    const text = await response.data.text()
+    const newElement = document.createElement('div')
+    newElement.className = "Message"
+    newElement.innerText = text
+    // console.log(newElement)
+    messagesElement.appendChild(newElement)
+}
