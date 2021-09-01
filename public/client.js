526ddab4aab78abca9da5e005e1066f8702eb7ea
Evgeniy <ekukonen@gmail.com>
2021-09-01 22:07:26 +0300

    add write files

--- public/client.js
+++ public/client.js
@@ -1,20 +1,34 @@
 const messagesElement = document.getElementById("Messages")
 const textElement = document.getElementById("Input-text")
 const buttonElemet = document.getElementById("Input-button")
+const fileBlockElement = document.getElementById("Input-file-block")
+const fileInputElement = document.getElementById("Input-file-input")
 let text = ""
+let file = ""
 
 const ws = new WebSocket('ws://localhost:8080')
 
 buttonElemet.addEventListener('click', event => {
     event.preventDefault();
+    console.log(file)
     text = textElement.value
-    ws.send(text)
+    // ws.send(text)
+    ws.send(file)
     textElement.value = ""
+})
+
+fileBlockElement.addEventListener('click', () => {
+    fileInputElement.click()
+})
+
+fileInputElement.addEventListener('change',getFile => {
+    file = getFile.target.files[0]
 })
 
 ws.onopen = () => setStatus('ONLINE')
 ws.onclose = () => setStatus('DISCONECTED')
 ws.onmessage = async (response) => {
+    console.log(response)
     const text = await response.data.text()
     const newElement = document.createElement('div')
     newElement.className = "Message"
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const WebSocket = require('ws')
-
+const fs = require('fs')
 const server = new WebSocket.Server({port: 8080})
 
 server.on('connection', ws => {
     ws.on('message', message => {
+        fs.writeFile('./static/' + 'some.jpg', message, ()=>{})
         server.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(message)
